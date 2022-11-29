a39840c2a0e0f606b7b34a0ad328c2421d98ade0
mystique09 <knightkrypto7@gmail.com>
2022-11-30 02:11:28 +0800

    add: cmd directory for main file

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"voidmanager/bot"
+	"voidmanager/utils"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	config, err := utils.LoadConfig(".", "app")
+	if err != nil {
+		log.Fatalf("[ERROR] unable to load config file, %v", err.Error())
+	}
+
+	benny := bot.New(&config)
+	benny.SetupIntents(discordgo.IntentsGuildMessages)
+	benny.AddCommandHandlers()
+	benny.StartBot()
+}
