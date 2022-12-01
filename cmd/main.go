7913c7fb9e501469ca7d92c28dc0900644acf328
mystique09 <knightkrypto7@gmail.com>
2022-12-01 15:36:45 +0800

    add: dbClient

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"voidmanager/bot"
+	"voidmanager/db"
 	"voidmanager/utils"
 
 	"github.com/bwmarrin/discordgo"
@@ -14,7 +15,9 @@ func main() {
 		log.Fatalf("[ERROR] unable to load config file, %v", err.Error())
 	}
 
-	benny := bot.New(&config)
+	dbClient := db.InitDB(&config)
+
+	benny := bot.New(&config, dbClient)
 	benny.SetupIntents(discordgo.IntentsGuildMessages)
 	benny.AddHandlers()
 	benny.StartBot()
