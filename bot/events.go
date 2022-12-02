ed4be33b8e1bb123019ff583f93ec7f253d38fb6
mystique09 <knightkrypto7@gmail.com>
2022-12-03 00:32:24 +0800

    feat: Events struct that will hold all events methods

--- bot/events.go
+++ bot/events.go
@@ -6,61 +6,122 @@ import (
 	"log"
 	"strings"
 	"voidmanager/db/ent"
+	"voidmanager/utils"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-func (h *Handler) Ready(s *discordgo.Session, r *discordgo.Ready) {
-	fmt.Printf("\nLogged in as %v \n", r.User.Username)
-	fmt.Println("Token: <redacted>")
-	fmt.Printf("Version: %v \n", h.cfg.BotVersion)
-	fmt.Printf("Onwer: %v \n", h.cfg.BotOwner)
-	fmt.Printf("Application ID: %v \n", h.cfg.BotApplicationId)
-	fmt.Printf("Guild ID: %v \n\n", h.cfg.BotGuildId)
+type Events struct {
+	client   *ent.Client
+	cfg      *utils.Config
+	commands *Commands
 }
 
-func (h *Handler) MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+func (e *Events) Ready(s *discordgo.Session, r *discordgo.Ready) {
+	allGuilds, err := e.client.Guild.Query().All(context.Background())
+
+	for _, guild := range allGuilds {
+		e.commands.prefixes[guild.ID] = guild.BotPrefix
+	}
+
+	log.Printf("Added prefixes %v", e.commands.prefixes)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	fmt.Printf("\nLogged in as %v \n", r.User.Username)
+	fmt.Println("Token: <redacted>")
+	fmt.Printf("Version: %v \n", e.cfg.BotVersion)
+	fmt.Printf("Onwer: %v \n", e.cfg.BotOwner)
+	fmt.Printf("Application ID: %v \n", e.cfg.BotApplicationId)
+	fmt.Printf("Guild ID: %v \n", e.cfg.BotGuildId)
+	fmt.Printf("Total commands: %d\n", len(commandHandlers))
+	fmt.Printf("Total slash commands: %d\n\n", len(slashCommandHandlers))
+}
+
+func (e *Events) MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
 	command, args := splitMessageContent(m.Content)
 
-	if strings.HasPrefix(command, "!") {
-		log.Printf("user <@%v> used the %v command with args %v", m.Author.ID, command, args)
+	guildPrefix, ok := e.commands.prefixes[m.GuildID]
+	if !ok {
+		if strings.HasPrefix(command, "!") {
+			command = strings.TrimPrefix(command, "!")
+			log.Printf("user <@%v> used the %v command with args %v", m.Author.ID, command, args)
+
+			cmd, ok := commandHandlers[command]
+			if !ok {
+				log.Println("command doesn't exist")
+				return
+			}
+			cmd(s, m, args, e.commands)
+		}
+
+		return
 	}
 
-	if command == "!ping" {
-		h.PingCommand(s, m, args)
+	if strings.HasPrefix(command, guildPrefix) {
+		command = strings.TrimPrefix(command, guildPrefix)
+		log.Printf("user <@%v> used the %v command with args %v", m.Author.ID, command, args)
+
+		cmd, ok := commandHandlers[command]
+		if !ok {
+			log.Println("command doesn't exist")
+			return
+		}
+		cmd(s, m, args, e.commands)
 	}
 }
 
-func (h *Handler) MemberCreate(s *discordgo.Session, newMember *discordgo.GuildMemberAdd) {
-	_, err := h.client.User.
+func (e *Events) GuildCreate(s *discordgo.Session, newGuild *discordgo.GuildCreate) {
+	_, err := e.client.Guild.
 		Create().
-		SetID(newMember.User.ID).
-		SetName(newMember.User.Username).
+		SetID(newGuild.ID).
+		SetBotPrefix("!").
+		Save(context.Background())
+
+	if err != nil {
+		log.Println("guild already exist")
+	}
+
+	log.Printf(`New guild %s added`, newGuild.ID)
+}
+
+func (e *Events) GuildRemove(s *discordgo.Session, guild *discordgo.GuildDelete) {
+	if err := e.client.Guild.DeleteOneID(guild.ID).Exec(context.Background()); err != nil {
+		log.Printf("guild %v doesn't exist in database", guild.ID)
+	}
+}
+
+func (e *Events) MemberCreate(s *discordgo.Session, newMember *discordgo.GuildMemberAdd) {
+	_, err := e.client.User.
+		Create().
+		SetID(newMember.Member.User.ID).
+		SetName(newMember.Member.User.Username).
 		Save(context.Background())
 
 	if err != nil {
 		log.Printf("user %v already exist", newMember.User.ID)
 	}
+
+	log.Printf(`New guild %s added`, newMember.Member.User.ID)
 }
 
-func (h *Handler) MemberRemove(s *discordgo.Session, member *discordgo.GuildMemberRemove) {
-	if err := h.client.User.DeleteOne(&ent.User{
-		ID:   member.User.ID,
-		Name: member.User.Username,
-	}).Exec(context.Background()); err != nil {
-		log.Printf("user %v doesn't exist in database", member.User.ID)
+func (e *Events) MemberRemove(s *discordgo.Session, member *discordgo.GuildMemberRemove) {
+	if err := e.client.User.DeleteOneID(member.Member.User.ID).Exec(context.Background()); err != nil {
+		log.Printf("user %v doesn't exist in database", member.Member.User.ID)
 	}
 }
 
-func splitMessageContent(content string) (prefix string, args []string) {
+func splitMessageContent(content string) (command string, args []string) {
 	splitContent := strings.Split(content, " ")
 
-	prefix = splitContent[0]
+	command = splitContent[0]
 	args = splitContent[1:]
 
-	return prefix, args
+	return command, args
 }
